c59f76b282e1a3ccadecc275a1727b1baf31a958
Dennis Heinrich <heinrich.himbergen@gmail.com>
2016-08-02 17:43:42 +0200

    Projekt hochgeladen
    - Programm erstellt & hochgeladen
    -- Das Programm prüft ob ein angegebener Prozess öfters als erlaubt
    läuft und schließt diesen anhand der letzten Ausführung.

---
+++ Prozessanzahl/Prozessmanager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prozessanzahl
+{
+    public partial class Prozessmanager : Form
+    {
+        public Prozessmanager(string[] args)
+        {
+            InitializeComponent();
+
+            if (args.Length > 3)
+            {
+                int maximaleAusfuehrungen = Convert.ToInt32(args[0]);
+                int checkInterval = Convert.ToInt32(args[1]);
+                string prozessname = args[2];
+                string errormessage = args[3];
+
+                while (true)
+                {
+                    Process[] ps = Process.GetProcessesByName(prozessname);
+
+                    if (ps.Length > maximaleAusfuehrungen)
+                    {
+                        int lastNum = ps.Length - maximaleAusfuehrungen;
+                        var last = (from p in ps orderby p.StartTime descending select p).Take(lastNum);
+
+                        foreach (Process p in last)
+                        {
+                            p.Kill();
+                        }
+
+                        MessageBox.Show(errormessage);
+                    }
+
+                    System.Threading.Thread.Sleep(checkInterval);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Die Prozessüberwachung erfordert mehr Parameter");
+            }
+        }
+    }
+}
